b7aea03997684257a372eaa9f1d312907c328a01
alex <stepurko.alexandr@gmail.com>
2022-04-14 20:21:05 +0300

    lesson 27 classwork

---
+++ winter2022/mvnProjects/liquibase/src/main/java/com/liqubase/dao/StudentDao.java
@@ -0,0 +1,17 @@
+package com.liqubase.dao;
+
+import com.liqubase.HibernateUtil;
+import com.liqubase.data.Student;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+
+import java.util.List;
+
+public class StudentDao {
+    Session session = HibernateUtil.getSessionFactory().openSession();
+
+    public List<Student> getAllCities() {
+        Criteria criteria = session.createCriteria(Student.class);
+        return criteria.list();
+    }
+}
